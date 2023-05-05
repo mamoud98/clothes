c903b13f6f0c9aceb3e9339161e02040a069215c
Mahmoud shorafa <m2shorafa@gmail.com>
2023-05-05 22:06:47 +0300

    using redux-persist

--- src/index.js
+++ src/index.js
@@ -5,15 +5,17 @@ import { RouterProvider } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import routes from "./routes";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import { store } from "./store/store";
+import { persistor, store } from "./store/store";
 import "./index.scss";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={routes} />
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={routes} />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
